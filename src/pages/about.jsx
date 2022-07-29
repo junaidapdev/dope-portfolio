259b655c832761d70d54afab67640cf3fb63f60b
junaidapdev <junaidap.dev.com>
2022-07-29 16:35:31 +0530

    testing

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,7 +4,7 @@ import Footer from '../sections/Footer'
 const About = () => {
   return (
     <div>
-       <h3 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black">
+       <h3 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-white">
         About --- Coming Soon
       </h3>
       
